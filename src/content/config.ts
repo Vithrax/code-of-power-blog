bb40327d215098e553e8f3b360d8d38fa2de9e28
Vithrax <adrian.gajdulewicz@outlook.com>
2024-02-26 14:51:30 +0100

    init commit

---
+++ src/content/config.ts
@@ -0,0 +1,32 @@
+import { z, defineCollection, type Render } from 'astro:content';
+
+const postSchema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string(),
+  image: z
+    .object({
+      url: z.string(),
+      alt: z.string(),
+    })
+    .optional(),
+  tags: z.array(z.string()),
+});
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: postSchema,
+});
+
+export const collections = {
+  posts: postsCollection,
+};
+
+// for typesafety in PostLayout
+export type Post = {
+  id: string;
+  slug: string;
+  body: string;
+  collection: 'pages';
+  data: z.infer<typeof postSchema>;
+} & { render(): Render['.md'] };
---
+++ src/content/posts/welcome-post.md
@@ -0,0 +1,11 @@
+---
+title: 'Welcome to code of Power'
+pubDate: 2024-02-26
+description: 'My personal blog about various things that i know a thing or two.'
+tags: ['general']
+---
+
+### Hello World
+
+This is the first post of my new blog called Code of Power
+Here i will post my thoughts and try to use it to spread my infinite widsom :-)
