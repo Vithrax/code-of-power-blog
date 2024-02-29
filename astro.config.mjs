4b3797cc41c70f8375e69a0f46e00d9a14fa6efb
Vithrax <adrian.gajdulewicz@outlook.com>
2024-02-29 11:12:44 +0100

    fix: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: false,
+  }),
 });
