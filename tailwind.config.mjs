04493da56715b4732ba728bb244ae85bf6d0e907
Vithrax <adrian.gajdulewicz@outlook.com>
2024-02-29 10:43:14 +0100

    feature: updated logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['Bebas Neue', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
